9e0b44598dd0687f6803358404c20cbaf301cfbf
KubikZ <jakubszewczyk999@gmail.com>
2021-11-14 01:06:52 +0000

    Improved adding, most of the tests passed

--- TerraCalc.cpp
+++ TerraCalc.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sstream>
 #include "aritmetic.h"
-bool AddTest();
 
+bool AddTest();
 int main()
 {
-	AddTest();
-	
+	std::cout << Aritmetic::Add("99", "1");
+
+	/*if(AddTest())
+		std::cout << "Tests successful!";
+	else
+		std::cout << "Tests failed!";
+*/
 	return 0;
 }
 
 bool AddTest()
 {
-	for(int i = 120; i < 130; i++)
-		for(int j = 120; j < 130; j++)
+	for(int i = 1; i < 100; i++)
+		for(int j = 1; j < 100; j++)
 		{
-			//if (atoi(Aritmetic::Add(std::to_string(i), std::to_string(j)).c_str()) != i+j)
-			std::cout<< i << "+" << j << "= " << Aritmetic::Add(std::to_string(i), std::to_string(j)) << " [" << i + j << ']'<< std::endl;
-			//std::cout << Aritmetic::Add("123", "78");
+			std::cout << i << " + " << j << std::endl;
+			std::string aIn = std::to_string(i);
+			std::string bIn = std::to_string(j);
+			std::string result = Aritmetic::Add(aIn, bIn);
+			//short resultNum = stoi(result);
+			//if (resultNum != i+j)
+			//	return false;
 		}
 	return true;
 }
--- aritmetic.h
+++ aritmetic.h
@@ -1,22 +1,24 @@
 #include <math.h>
 #include <string>
 
+#define CHR_TO_SHR(chr) (short)(chr - '0')
+
 namespace Aritmetic
 {
     // 123, 78
     std::string add(std::string a, std::string b)
     {
         std::string result = "";
-        
+
         size_t aDigits = a.size();
         size_t bDigits = b.size();
-        short carry = 0; 
+        short carry = 0;
         size_t index = 0;
         while(index < bDigits)
         {
           size_t backIndex = bDigits - index - 1;
-          short sum = (short)(a[backIndex + 1] - '0') + (short)(b[backIndex] - '0');
-          
+          short sum = CHR_TO_SHR(a[backIndex + (aDigits - bDigits)]) + CHR_TO_SHR(b[backIndex]);
+
           sum += carry;
           carry = sum/10;
           char digit;
@@ -24,19 +26,18 @@ namespace Aritmetic
             digit = sum - 10;
           else
             digit = sum;
-            
+
           result.insert(result.begin(), '0' + digit);
           index++;
         }
-        if(aDigits > bDigits)
-        {
-          std::string rest = a.substr(0, a.size() - b.size());
-          rest[rest.size()-1] += carry; 
-          result.insert(result.begin(), rest.begin(), rest.end());
-        }
+
+        std::string rest = a.substr(0, aDigits - bDigits);
+        rest[rest.size()-1] += carry;
+        result.insert(result.begin(), rest.begin(), rest.end());
+
         return result;
     }
-  
+
     std::string Add(std::string a1, std::string a2)
     {
         if (a1.size() >= a2.size())
